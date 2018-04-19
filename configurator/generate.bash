5d462ddb316c201d286b04aa5e6bbb5f79c38802
Michael J. Brim <brimmj@ornl.gov>
2018-04-19 09:56:49 -0400

    configurator - add generate script

---
+++ configurator/generate.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function usage_error {
+    echo "ERROR: USAGE - $0 prefix"
+    exit 1
+}
+
+function file_error {
+    echo "ERROR: $0 - missing necessary file $1"
+    exit 2
+}
+
+function cmd_error {
+    echo "ERROR: $0 - command failed: $*"
+    exit 3
+}
+
+if [[ $# -ne 1 ]]; then
+    usage_error
+fi
+
+pref=$1
+upref=$(echo $pref | tr '[a-z]' '[A-Z]')
+lpref=$(echo $pref | tr '[A-Z]' '[a-z]')
+
+# get source directory
+sdir=$(dirname $0)
+
+# find source files
+doth=$sdir/configurator.h
+dotc=$sdir/configurator.c
+[[ -f $doth ]] || file_error $doth
+[[ -f $dotc ]] || file_error $dotc
+
+# create new files with given prefix, and substituting prefix
+sed_cmd="sed -e s/PREFIX/$upref/g -e s/prefix/$lpref/g"
+echo "DEBUG: sed_cmd - $sed_cmd"
+cmd="$sed_cmd $doth > ./${lpref}_configurator.h"
+echo "DEBUG: cmd - $cmd"
+$sed_cmd $doth > ./${lpref}_configurator.h || cmd_error $cmd
+
+cmd="$sed_cmd $dotc > ./${lpref}_configurator.c"
+$sed_cmd $dotc > ./${lpref}_configurator.c || cmd_error $cmd
+
+exit 0
+
